ff114b76837aaff78225115280bb5f2b3b18856e
Nabil BENDAFI <nabil@bendafi.fr>
2022-01-16 21:36:24 +0100

    fix: use Cthulu picture as fallback

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Handlebars = require("handlebars");
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	resume.basics.picture = resume.basics.picture ? resume.basics.picture : "http://css-tricks.com/examples/OnePageResume/images/cthulu.png";
 	return Handlebars.compile(template)({
 		css: css,
 		resume: resume
